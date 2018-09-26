a77400d399ce903e8ec1a8461d4b0cdcd3feb534
Hygor Zorak <hygorzorak@gmail.com>
2018-09-26 16:07:31 -0300

    Heroku build

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-import * as type from './types'
-
-export const getPosts = posts => ({type: type.GET_POSTS, posts})
-
-export const getPostById = currentPost => ({type: type.GET_POST_BY_ID, currentPost})
-
-// export const fetchPosts = () => dispatch => (api.getPosts().then(res => dispatch(getPosts({posts: res}))))
-
-// export const fetchPostById = postId => dispatch => (api.getPostById(postId).then(res => dispatch(getPostById(res))))
